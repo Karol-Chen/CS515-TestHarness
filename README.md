28f946ef132ee2dbaf286711c8c9222b7dc1221a
Daixuan Chen <DaixuanChenShaw@gmail.com>
2023-11-19 23:46:05 -0500

    test

--- README.md
+++ README.md
@@ -13,7 +13,7 @@
 
 ## Time Spent
 
-- I spent approximately 20 hours on this project.
+- I spent approximately 20 hours on this project. About 10 of those hours I spent writing gron and ungron, and about 2 hours of the other 10 hours consulting the documentation.
 
 ## Testing Approach
 
@@ -23,11 +23,12 @@
 
 ## Bugs or Issues
 
--
+- I encountered tons of bugs in gron.py, such as stack overflows due to recursive repeat calls, and overcalls in processing json files.
+- The main problems I'm having with test.py are inaccessible files and subprocess handling issues. The former I solved by modifying relative paths, the latter I did in hardcode form.
 
 ## Difficult Issue
 
-- One challenging issue was related to handling
+- The most challenging issue I encountered was related to the inability to locate files when running tests in the GitHub Actions environment. The root cause of this problem lies in the difference between the working directory in the GitHub Actions environment and the one set locally, causing file paths to be resolved incorrectly.
 
 ## Implemented Extensions
 
@@ -41,6 +42,6 @@
    - Description: The `wc` program now supports flags to control the information shown. For example, `-l` counts only lines, `-w` counts only words, and `-c` counts only characters. Multiple flags can be combined.
    - Evaluation: Test different combinations of flags with various input files and ensure that the output matches the expected counts.
 
-3. **Expected exit status**
-   - Description: Each test case now includes a `PROG.NAME.status` file that contains a valid exit status (0 through 255). The test harness checks if the actual exit status matches the expected one.
-   - Evaluation: Create test cases with various exit statuses and verify that the program correctly exits with the expected status.
+3. **Expected STDERR**
+   - Description: Introduce the ability to specify the expected output on STDERR using a PROG.NAME.err file. If the STDERR of the actual command doesn’t match, fail the test case. Also, consider creating PROG.NAME.arg.err for running on arguments.
+   - Evaluation: Write tests with both empty and non-empty STDERR and PROG.NAME.err files, ensuring that the test case fails when STDERR doesn't match the expected output.
--- test.py
+++ test.py
@@ -1,67 +1,3 @@
-# import os
-# import subprocess
-# os.chdir(os.path.dirname(os.path.abspath(__file__)))
-# def run_test(program, test_name):
-#     input_file = f"test/{program}.{test_name}.in"
-#     print(input_file)
-#     expected_output_file = f"test/{program}.{test_name}.out"
-#     expected_arg_output_file = f"test/{program}.{test_name}.arg.out"
-#     args=None
-#     if(program=="gron"):
-#         args=f"python prog/{program}.py -f {input_file}"
-#     if(program=="wc"):
-#         args=f"python prog/{program}.py -l -w -c {input_file}"
-#     if(program=="csv_sum"):
-#         args=f"python prog/{program}.py {input_file} age,salary"
-#     try:
-#         with open(input_file, 'r') as f:
-#             input_data = f.read()
-#         process = subprocess.Popen(args, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
-#         output, _ = process.communicate(input=input_data)
-#         exit_code = process.returncode
-
-#         if os.path.exists(expected_output_file):
-#             with open(expected_output_file, 'r') as f:
-#                 expected_output = f.read()
-#                 assert output == expected_output, "OutputMismatch"
-
-#         if os.path.exists(expected_arg_output_file):
-#             process = subprocess.Popen(args, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
-#             arg_output, _ = process.communicate()
-#             exit_code_arg = process.returncode
-#             with open(expected_arg_output_file, 'r') as f:
-#                 expected_arg_output = f.read()
-#                 assert arg_output == expected_arg_output, "OutputMismatch"
-
-#         assert exit_code == 0, "NonZeroExitCode"
-
-#         assert exit_code_arg == 0, "NonZeroExitCode_Arg"
-
-#         print(f"OK: {program} {test_name}")
-
-#     except Exception as e:
-#         print(f"FAIL: {program} {test_name} ({e})")
-#             # print(f"FAIL: {program} {test_name} ({e})")
-#         print(f"Command: {args}")
-#         print(f"Output: {output}")
-#         print(f"Exit Code: {exit_code}")
-#     # finally:
-#     #     os.chdir("..") 
-
-# def run_tests(program):
-#     test_files = [f for f in os.listdir("./test") if f.startswith(f"{program}.") and f.endswith(".in")]
-
-#     print(test_files)
-#     for test_file in test_files:
-#         test_name = test_file[len(program) + 1:-3]
-#         run_test(program, test_name) 
-
-# if __name__ == "__main__":
-#     program_name = "gron"
-#     run_tests(program_name)
-#     run_tests("csv_sum")
-#     run_tests("wc")
-
 import os
 import subprocess
 
@@ -69,10 +5,11 @@ os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
 def run_test(program, test_name):
     input_file = os.path.join("test", f"{program}.{test_name}.in")
-    print(input_file)
     expected_output_file = os.path.join("test", f"{program}.{test_name}.out")
     expected_arg_output_file = os.path.join("test", f"{program}.{test_name}.arg.out")
-    
+    expected_err_output_file = os.path.join("test", f"{program}.{test_name}.err")
+    expected_arg_err_output_file = os.path.join("test", f"{program}.{test_name}.arg.err")
+
     args = None
     if program == "gron":
         args = f"python prog/{program}.py -f {input_file}"
@@ -80,12 +17,13 @@ def run_test(program, test_name):
         args = f"python prog/{program}.py -l -w -c {input_file}"
     elif program == "csv_sum":
         args = f"python prog/{program}.py {input_file} age,salary"
-    
+
     try:
         with open(input_file, 'r') as f:
             input_data = f.read()
+
         process = subprocess.Popen(args, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
-        output, _ = process.communicate(input=input_data)
+        output, err_output = process.communicate(input=input_data)
         exit_code = process.returncode
 
         if os.path.exists(expected_output_file):
@@ -101,6 +39,19 @@ def run_test(program, test_name):
                 expected_arg_output = f.read()
                 assert arg_output == expected_arg_output, "OutputMismatch"
 
+        if os.path.exists(expected_err_output_file):
+            with open(expected_err_output_file, 'r') as f:
+                expected_err_output = f.read()
+                assert err_output.strip() == expected_err_output.strip(), "ErrorOutputMismatch"
+
+        if os.path.exists(expected_arg_err_output_file):
+            process = subprocess.Popen(args, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+            _, arg_err_output = process.communicate()
+            exit_code_arg = process.returncode
+            with open(expected_arg_err_output_file, 'r') as f:
+                expected_arg_err_output = f.read()
+                assert arg_err_output.strip() == expected_arg_err_output.strip(), "ErrorOutputMismatch"
+
         assert exit_code == 0, "NonZeroExitCode"
         assert exit_code_arg == 0, "NonZeroExitCode_Arg"
 
@@ -110,6 +61,7 @@ def run_test(program, test_name):
         print(f"FAIL: {program} {test_name} ({e})")
         print(f"Command: {args}")
         print(f"Output: {output}")
+        print(f"Error Output: {err_output}")
         print(f"Exit Code: {exit_code}")
 
 def run_tests(program):
