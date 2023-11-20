91486a800153400d228801b6bc00efd240beef6f
Daixuan Chen <DaixuanChenShaw@gmail.com>
2023-11-19 22:24:56 -0500

    modify test.py

--- tes.py
+++ tes.py
@@ -1,4 +1,8 @@
-with open("./test/gron.data1.in") as file:
+with open("./test/gron.data2.in","r") as file:
     print(open)
     lines=file.read()
     print(lines)
+
+input_file="./test/gron.data2.in"
+with open(input_file, 'r') as f:
+    print("open file")
--- test.py
+++ test.py
@@ -1,80 +1,30 @@
-# import os
-# import subprocess
-
-# def run_test(program, test_name):
-#     input_file = f"./test/{program}.{test_name}.in"
-#     expected_output_file = f"./test/{program}.{test_name}.out"
-#     expected_arg_output_file = f"./test/{program}.{test_name}.arg.out"
-#     print(input_file)
-#     try:
-#         print("line10")
-#         process = subprocess.Popen([program], stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-#         print(f"Process opened: {process}")
-#         print("ready to open file")
-#         with open(input_file, 'r') as f:
-#             print("open file")
-#             input_data = f.read()
-#         output, _ = process.communicate(input=input_data)
-#         exit_code = process.returncode
-
-#         if os.path.exists(expected_output_file):
-#             with open(expected_output_file, 'r') as f:
-#                 expected_output = f.read()
-#                 assert output == expected_output, "OutputMismatch"
-
-#         if os.path.exists(expected_arg_output_file):
-#             process = subprocess.Popen([program, input_file], stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
-#             arg_output, _ = process.communicate()
-#             exit_code_arg = process.returncode
-#             with open(expected_arg_output_file, 'r') as f:
-#                 expected_arg_output = f.read()
-#                 assert arg_output == expected_arg_output, "OutputMismatch"
-
-#         assert exit_code == 0, "NonZeroExitCode"
-
-#         assert exit_code_arg == 0, "NonZeroExitCode_Arg"
-
-#         print(f"OK: {program} {test_name}")
-
-#     except Exception as e:
-#         print(f"FAIL: {program} {test_name} ({e})")
-
-# def run_tests(program):
-#     test_files = [f for f in os.listdir("test") if f.startswith(f"{program}.") and f.endswith(".in")]
-
-#     for test_file in test_files:
-#         test_name = test_file[len(program) + 1:-3]
-#         run_test(program, test_name)
-
-# if __name__ == "__main__":
-#     program_name = "gron"
-#     run_tests(program_name)
 import os
 import subprocess
 
-def run_test(program, test_name, test_path):
+def run_test(program, test_name):
     input_file = f"./test/{program}.{test_name}.in"
     print(input_file)
     expected_output_file = f"./test/{program}.{test_name}.out"
     expected_arg_output_file = f"./test/{program}.{test_name}.arg.out"
+
+    args=f"python ./prog/{program}.py -f {input_file}"
     try:
-        print("line10")
-        print("process")
-        print("ready to open file")
         with open(input_file, 'r') as f:
-            print("open file")
             input_data = f.read()
-        process = subprocess.Popen([program], stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+        process = subprocess.Popen(args, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
         output, _ = process.communicate(input=input_data)
         exit_code = process.returncode
 
         if os.path.exists(expected_output_file):
+            print("过if了")
             with open(expected_output_file, 'r') as f:
+                print("open")
                 expected_output = f.read()
+                print(output)
                 assert output == expected_output, "OutputMismatch"
 
         if os.path.exists(expected_arg_output_file):
-            process = subprocess.Popen([program, input_file], stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+            process = subprocess.Popen(args, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
             arg_output, _ = process.communicate()
             exit_code_arg = process.returncode
             with open(expected_arg_output_file, 'r') as f:
@@ -89,8 +39,12 @@ def run_test(program, test_name, test_path):
 
     except Exception as e:
         print(f"FAIL: {program} {test_name} ({e})")
-    finally:
-        os.chdir("..") 
+            # print(f"FAIL: {program} {test_name} ({e})")
+        print(f"Command: {args}")
+        print(f"Output: {output}")
+        print(f"Exit Code: {exit_code}")
+    # finally:
+    #     os.chdir("..") 
 
 def run_tests(program):
     test_files = [f for f in os.listdir("./test") if f.startswith(f"{program}.") and f.endswith(".in")]
@@ -98,8 +52,7 @@ def run_tests(program):
     print(test_files)
     for test_file in test_files:
         test_name = test_file[len(program) + 1:-3]
-        test_path = os.path.join("test", test_file)
-        run_test(program, test_name, test_path) 
+        run_test(program, test_name) 
 
 if __name__ == "__main__":
     program_name = "gron"
